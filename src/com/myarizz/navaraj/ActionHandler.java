43cf5f616da60d6945005c319f44dddbfdd35afd
Navaraj Neupane <jalesa642@yahoo.com>
2015-02-28 12:22:07 +0100

    ActionHandler implemented.

--- src/com/myarizz/navaraj/ActionHandler.java
+++ src/com/myarizz/navaraj/ActionHandler.java
@@ -16,9 +16,54 @@ import java.util.Map;
 public class ActionHandler {
 	
 	/**
-	 * @param args
+	 * @param map
+	 * 
 	 * This method will receive list containing player's detail and calculate the points and send back to frontend.
-	 * TODO Define interface to receive input from frontend. Modify method return type to handle data to be sent to frontend.
+	 * 
+	 * @return map
+	 */
+	public static Map<String, Integer> sendInput(Map<String, List<Object>> map){
+		
+		int totalPoints = 0;
+		int gamerPoint = 0;
+		String winner = "";
+		Map<String, Integer> mapPlayerPoint = new HashMap<String, Integer>();
+		
+		for(Map.Entry<String, List<Object>> entry:map.entrySet()){
+			List<Object> values = entry.getValue();
+			int pointOfEachPlayer = (int) values.get(0); 
+			totalPoints += pointOfEachPlayer;
+		}		
+		
+		for(Map.Entry<String, List<Object>> entry:map.entrySet()){
+			
+			List<Object> values = entry.getValue();
+			int obtainedPoint = 0;
+			
+			if(values.get(1)=="seen" && values.get(2)!="winner"){
+				obtainedPoint = (int)values.get(0)*values.size()-(totalPoints +3); 
+				gamerPoint -=obtainedPoint;
+				System.out.println("Key:"+entry.getKey()+ " Obtained Point:"+obtainedPoint);
+				mapPlayerPoint.put(entry.getKey(), obtainedPoint);
+			}
+			else if(values.get(1)!="seen"){
+				obtainedPoint = -(totalPoints +10);
+				gamerPoint -=obtainedPoint;
+				mapPlayerPoint.put(entry.getKey(), obtainedPoint);
+				System.out.println("KeyNotSeen:"+entry.getKey()+ " Obtained Point:"+obtainedPoint);
+			}
+			else{
+				winner = entry.getKey();
+			}		
+		}
+		mapPlayerPoint.put(winner, gamerPoint);
+		System.out.println("KeyWinner:"+winner+ " Obtained Point:"+gamerPoint);
+		return mapPlayerPoint;
+	}
+	
+	/**
+	 * @param args
+	 * 
 	 */
 	public static void main(String[] args){
 		Map<String, List<Object>> map = new HashMap<String, List<Object>>();
@@ -34,25 +79,25 @@ public class ActionHandler {
 		player2.add(null);
 		
 		List<Object> player3 = new ArrayList<Object>();
-		player3.add(15);
-		player3.add("seen");
+		player3.add(0);
+		player3.add(null);
 		player3.add(null);
 		
-		map.put("Player1", player1);
-		map.put("Player2", player2);
-		map.put("Player3", player3);
+		map.put("A", player1);
+		map.put("B", player2);
+		map.put("C", player3);
 		
-		int totalPoints =0;
-		for(Map.Entry<String, List<Object>> entry:map.entrySet()){
+		Map<String, Integer> mapPlayerPoint =sendInput(map);
+		
+		for(Map.Entry<String, Integer> entry:mapPlayerPoint.entrySet()){
 			
 			String key = entry.getKey();
-			List<Object> values = entry.getValue();
-			int pointOfEachPlayer = (int) values.get(0); 
-			totalPoints += pointOfEachPlayer;
+			int pointOfEachPlayer = entry.getValue();
 			
 			System.out.println("Key:"+key);
-			System.out.println("Values:"+values);
+			System.out.println("Values:"+pointOfEachPlayer);
 		}
-		System.out.println("Total Points:"+totalPoints);
 	}
+	
+
 }
